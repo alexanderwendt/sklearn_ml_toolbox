ca7a5afec83c07fa64b569c88f024ed8d540e6d8
wendt <alexander.wendt@tuwien.ac.at>
2021-01-07 21:26:10 +0100

    Added files for samples removed debug setups from the real folders

---
+++ samples/omxs30debugi_run_step7_prediction_inference.bat
@@ -0,0 +1,34 @@
+echo #===========================================#
+echo # Alexander Wendts Machine Learning Toolbox #
+echo #===========================================#
+
+rem define config file to use
+set config_file="config/debug_timedata_omxs30_inference.ini"
+
+echo setup environment
+call conda activate env_ml
+
+echo #===========================================#
+echo # Generate Dataset #
+echo #===========================================#
+
+
+echo #===========================================#
+echo # Data Analysis and Preprocessing #
+echo #===========================================#
+
+
+echo #=================================================#
+echo # Model Training#
+echo #=================================================#
+
+
+echo #=================================================#
+echo # Model Evaluation for Temporal Datasets #
+echo #=================================================#
+
+
+echo #=================================================#
+echo # Prediction #
+echo #=================================================#
+python ..\step70_predict_temporal_data.py --config_path=%config_file%
